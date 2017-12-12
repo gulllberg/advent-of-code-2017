1e0a757784481d34fd7a99a3a7e64652c55ad918
Daniel Gullberg <daniel.gullberg@evry.com>
2017-12-12 19:15:38 +0100

    Added day 4

--- src/advent_of_code_2017/core.clj
+++ src/advent_of_code_2017/core.clj
@@ -1,5 +1,6 @@
 (ns advent-of-code-2017.core
-  (:import (jdk.internal.util.xml.impl Input)))
+  ;(:require [ysera.test :refer [is=]])
+  )
 
 ;; Day 1
 
@@ -254,4 +255,75 @@
   )
 
 
+;; Day 4
+
+(def input-4 (slurp "assets/input-4.txt"))
+
+;A new system policy has been put in place that requires all accounts to use a passphrase instead of simply a password. A passphrase consists of a series of words (lowercase letters) separated by spaces.
+;
+;To ensure security, a valid passphrase must contain no duplicate words.
+;
+;For example:
+;
+;aa bb cc dd ee is valid.
+;aa bb cc dd aa is not valid - the word aa appears more than once.
+;aa bb cc dd aaa is valid - aa and aaa count as different words.
+;The system's full passphrase list is available as your puzzle input. How many passphrases are valid?
+
+(defn valid-passphrase-a?
+  [passphrase]
+  (let [passphrase-list (clojure.string/split passphrase #" ")]
+    (= (count passphrase-list)
+       (->> passphrase-list
+            (set)
+            (into (vector))
+            (count)))))
+
+(defn problem-4a
+  [input]
+  (->> (clojure.string/split input #"\n")
+       (filter valid-passphrase-a?)
+       (count)))
+
+;For added security, yet another system policy has been put in place. Now, a valid passphrase must contain no two words that are anagrams of each other - that is, a passphrase is invalid if any word's letters can be rearranged to form any other word in the passphrase.
+;
+;For example:
+;
+;abcde fghij is a valid passphrase.
+;abcde xyz ecdab is not valid - the letters from the third word can be rearranged to form the first word.
+;a ab abc abd abf abj is a valid passphrase, because all letters need to be used when forming another word.
+;iiii oiii ooii oooi oooo is valid.
+;oiii ioii iioi iiio is not valid - any of these words can be rearranged to form any other word.
+;Under this new system policy, how many passphrases are valid?
+
+(defn valid-passphrase-b?
+  {:test (fn []
+           (clojure.test/is (not (valid-passphrase-b? "ab ba")))
+           (clojure.test/is (valid-passphrase-b? "ab ac")))}
+  [passphrase]
+  (let [passphrase-list (clojure.string/split passphrase #" ")]
+    (= (count passphrase-list)
+       (->> passphrase-list
+            (map sort)
+            (set)
+            (into (vector))
+            (count)))))
+
+(defn problem-4b
+  [input]
+  (->> (clojure.string/split input #"\n")
+       (filter valid-passphrase-b?)
+       (count)))
+
+(clojure.test/deftest day-4
+  (clojure.test/is (= (problem-4a input-4) 386))
+  (clojure.test/is (= (problem-4b input-4) 208)))
+
+(comment
+  (valid-passphrase-a? "aa bb bb")
+  (problem-4a input-4)
+  ;; 386
+  (problem-4b input-4)
+  ;; 208
+  )
 
