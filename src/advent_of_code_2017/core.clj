29193aee3c3342db2d39e8e7e8eb6a0b66cd7cab
Daniel Gullberg <daniel.gullberg@evry.com>
2017-12-27 18:51:10 +0100

    Added day 11

--- src/advent_of_code_2017/core.clj
+++ src/advent_of_code_2017/core.clj
@@ -768,3 +768,78 @@
   ; 6209
   )
 
+;; Day 11
+
+(def input-11 (slurp "assets/input-11.txt"))
+
+;The hexagons ("hexes") in this grid are aligned such that adjacent hexes can be found to the north, northeast, southeast, south, southwest, and northwest:
+;
+;\ n  /
+;nw +--+ ne
+;/    \
+;-+      +-
+;\    /
+;sw +--+ se
+;/ s  \
+;You have the path the child process took. Starting where he started, you need to determine the fewest number of steps required to reach him. (A "step" means to move from the hex you are in to any adjacent hex.)
+;
+;For example:
+;
+;ne,ne,ne is 3 steps away.
+;ne,ne,sw,sw is 0 steps away (back where you started).
+;ne,ne,s,s is 2 steps away (se,se).
+;se,sw,se,sw,sw is 3 steps away (s,s,sw).
+
+(defn problem-11-calculate-distance
+  [x y]
+  (let [x (max x (- x))
+        y (max y (- y))]
+    (if (>= x (* 2 y))
+      x
+      (int (+ x (- y (/ x 2)))))))
+
+(defn problem-11a
+  {:test (fn []
+           (is= (problem-11a "ne,ne,ne") 3)
+           (is= (problem-11a "ne,ne,sw,sw") 0)
+           (is= (problem-11a "se,sw,se,sw,sw") 3))}
+  [input]
+  (let [steps (-> (clojure.string/trim input)
+                  (clojure.string/split #","))
+        [x y] (reduce (fn [[x y] step]
+                        (condp = step
+                          "n" [x (inc y)]
+                          "ne" [(inc x) (+ y 0.5)]
+                          "nw" [(dec x) (+ y 0.5)]
+                          "s" [x (dec y)]
+                          "se" [(inc x) (- y 0.5)]
+                          "sw" [(dec x) (- y 0.5)]))
+                      [0 0]
+                      steps)]
+    (problem-11-calculate-distance x y)))
+
+;; How many steps away is the furthest he ever got from his starting position?
+
+(defn problem-11b
+  [input]
+  (let [steps (-> (clojure.string/trim input)
+                  (clojure.string/split #","))]
+    (->> steps
+         (reduce (fn [[x y max-distance] step]
+                   (let [[x y] (condp = step
+                                 "n" [x (inc y)]
+                                 "ne" [(inc x) (+ y 0.5)]
+                                 "nw" [(dec x) (+ y 0.5)]
+                                 "s" [x (dec y)]
+                                 "se" [(inc x) (- y 0.5)]
+                                 "sw" [(dec x) (- y 0.5)])]
+                     [x y (max max-distance (problem-11-calculate-distance x y))]))
+                 [0 0 0])
+         (last))))
+
+(comment
+  (problem-11a input-11)
+  ;; 747
+  (problem-11b input-11)
+  ;; 1544
+  )
